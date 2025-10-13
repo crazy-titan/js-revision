26ccdf232b829ffffe44415e16a6616ef1a44dfd
Abhinav Anand <144576369+crazy-titan@users.noreply.github.com>
2025-10-13 17:52:41 +0000

    add datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+// there are many new changes inside JS, if you want to use the new syntax and standards(new version)
+"use strict"; 
+
+
+// alert(3+3); not possible because we are using nodejs not the browser..
+
+console.log(3+3) ; console.log(3-2);   // there should be code readablity otherwise its just not make sense
+// code should be clean and eyecatching to the reviewers.
+
+let name = "titan"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to the power 53.
+// bigInt
+
+// boolean ==> true and false
+// string ==> "Titan"
+// null ==> standalone value
+// undefined ==> undefined
+// symbol ==> unique representation(react)
+
+console.log(name);
+
+// typeof to get the type of datatypes
+
+console.log(typeof null); // its an object , language problem or its just how this language works,
+console.log(typeof undefined); // its undefined
+
+// keep in mind some useful website, mdN and ecmaScript(different brower has different rules so to make everything looks same
+// a group wad formed to make certain standards.)
