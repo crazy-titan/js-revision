63ff2f94376c4f53a0206fa29f17984b5e9fc1aa
Abhinav Anand <144576369+crazy-titan@users.noreply.github.com>
2025-10-15 14:44:20 +0000

    add operation of strings

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,3 +30,42 @@ console.log(typeof valueInBoolean);
 console.log(valueInBoolean);
 
 // output will be boolean and true. so we can say 1 is treated as True and 0 is treated as false.
+
+
+//  ***************************** Operations *************************************
+
+// Basics operations addition,subtration,multiplication,division,remainder,power
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 ** 2);
+console.log(2 / 2);
+console.log(2 % 2);
+
+// These are the basics operation, where as % is important used in cryptography
+
+// meme js concepts 
+console.log("1"+2);    // 12 because first "" will make everything in string
+console.log(1+"2");   // 12 but here first is number and than it will stick it like string
+
+console.log("1"+2+2); // 122 beacuse first is "" so everything will act as string
+console.log(1+2+"2"); // 32 beacuse first is number and then it will stick it like string
+
+
+// more concepts like increament (prefix and postfix) checkout the MDN docs for more understanding
+let value = true;
+console.log((+true)); // 1 it will take as number
+// console.log(true+); // error not possible
+
+let score1 = 100;
+console.log(++score1); // 101
+console.log(score1); // 
+
+let score2 = 102;
+console.log(score2++); // 102
+console.log(score2);
+
+
+
+
